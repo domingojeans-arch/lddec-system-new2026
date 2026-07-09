ee7ae2be81b68aaea8b77619bfee67150fb7cb72
Gravity Denim <gravitydenim@gmail.com>
2026-07-09 09:20:04 -0500

    style: aumentar tamaño de letra y grosor de negritas para el LOTE N°

--- src/components/ingresos/print-sheets.tsx
+++ src/components/ingresos/print-sheets.tsx
@@ -407,10 +407,10 @@ function SingleSheetView({
               </td>
             </tr>
 
-            {/* Fila 3: LOTE, MUESTRA EXTERNA y N° PRENDAS 1 */}
+            {/* Fila 3: LOTE y N° PRENDAS 1 */}
             <tr style={{ height: "30px" }}>
               <td colSpan={1} style={{ ...cellStyle, width: "10%", borderLeft: "none" }}>LOTE N°</td>
-              <td colSpan={5} style={{ ...cellStyle, fontSize: "16px", fontWeight: "bold", textAlign: "center", color: "black" }}>
+              <td colSpan={8} style={{ ...cellStyle, fontSize: "24px", fontWeight: "900", textAlign: "center", color: "black" }}>
                 {lote || ""}
               </td>
               <td colSpan={3} rowSpan={2} style={{ ...cellStyle, verticalAlign: "top", padding: "3px 5px" }}>
